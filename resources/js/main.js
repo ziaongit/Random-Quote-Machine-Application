a1ecaf7e884fe2ecfdaa9ebcd0f2971b428665a3
Zia <zia_gt@yahoo.com>
2017-07-31 02:28:21 +0200

    Share on Twitter mock and function added

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,30 +1,38 @@
 var randomQuote;
 var currentIndex;
+var quoteText;
+var quoteAuthor;
 $(document).ready(function() { 
     getRandomQuote();
     $('#randomQuote').click(getRandomQuote);
+    $('#twitter').click(shareTweet);
+    
+    function getRandomQuote(){
+        $.getJSON("https://talaikis.com/api/quotes/", function(data) {
+        generateQuote(data);
+        });
+    }
+
+    function generateQuote(quotes) {
+        do {
+            var QuotesIndex = Math.floor(Math.random() * quotes.length);
+        }while(currentIndex === QuotesIndex)
+        
+        randomQuote = quotes[QuotesIndex];
+        quoteText = randomQuote.quote;
+        quoteAuthor = randomQuote.author;
+        currentIndex = QuotesIndex;
+        
+        $('#quote').html(randomQuote.quote);
+        $('#author').html(randomQuote.author);
+
+        
+    }
+    function shareTweet() {
+
+        window.open('https://twitter.com/intent/tweet?text='+quoteText+' - '+quoteAuthor);
+    }
   
 });
 
 
-// Get Random Quote start
-function getRandomQuote(){
-    $.getJSON("https://talaikis.com/api/quotes/", function(data) {
-    generateQuote(data);
-    });
-}
-// Get Random Quote end
-
-// Generate random Quote start
-function generateQuote(quotes) {
-    do {
-        var QuotesIndex = Math.floor(Math.random() * quotes.length);
-    }while(currentIndex === QuotesIndex)
-    
-    randomQuote = quotes[QuotesIndex];
-    currentIndex = QuotesIndex;
-    
-    $('#quote').html(randomQuote.quote);
-    $('#author').html(randomQuote.author);
-}
-// Generate random Quote end
